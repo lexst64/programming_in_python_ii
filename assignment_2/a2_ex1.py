6ffaf81fcc6d2c8898b9fed4de1c0b328f9deb86
lexst64 <alexey.st.official@gmail.com>
2024-03-25 19:11:20 +0100

    comments and docs added to to_grayscale()

--- assignment_2/a2_ex1.py
+++ assignment_2/a2_ex1.py
@@ -9,23 +9,30 @@ def to_grayscale(pil_image: np.ndarray) -> np.ndarray:
     if pil_image.shape[2] != 3:
         raise ValueError('Image\'s 3rd dimension does not have a size 3')
 
-    norm_image = pil_image / 255
+    # we assume pil_image has values in [0, 255] and normalize them to [0, 1]
+    rgb_norm = pil_image / 255
 
-    rgb_linear = np.where(norm_image <= 0.04045,
-                          norm_image / 12.92,
-                          ((norm_image + 0.055) / 1.055)**2.4)
+    # apply gamma expansion
+    rgb_linear = np.where(rgb_norm <= 0.04045,
+                          rgb_norm / 12.92,
+                          ((rgb_norm + 0.055) / 1.055)**2.4)
 
+    # leave linear R, G and B channels separate in (H, W, 1) shaped ndarrays
     r_linear = np.delete(rgb_linear, [1, 2], axis=2)
     g_linear = np.delete(rgb_linear, [0, 2], axis=2)
     b_linear = np.delete(rgb_linear, [0, 1], axis=2)
 
+    # calculate linear luminance
     y_linear = 0.2126 * r_linear + 0.7152 * g_linear + 0.0722 * b_linear
 
+    # get linear luminance back to a non-linear representation
+    # by the inverse of the gamma expansion
     y = np.where(y_linear <= 0.0031308,
                  12.92 * y_linear,
                  1.055 * (y_linear ** (1/2.4)) - 0.055)
 
-    y_reshaped = y.reshape((y.shape[2], y.shape[0], y.shape[1]))
+    # shape to (1, H, W)
+    y_reshaped = y.reshape((1, y.shape[0], y.shape[1]))
 
     if np.issubdtype(pil_image.dtype, np.integer):
         return y_reshaped.round(0).astype(pil_image.dtype)
